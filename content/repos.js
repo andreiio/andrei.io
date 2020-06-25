c7940f20e77290015185c564c077f430225334ad
Andrei Ioniță <hi@andrei.io>
2020-06-25 22:23:40 +0100

    Fix indentation and update dependencies (#4)

--- content/repos.js
+++ content/repos.js
@@ -1,50 +1,50 @@
 export default [
-	{
-		repo: 'code4romania/cms',
-		description: `Preconfigured CMS with a modular layout`,
-		primaryLanguage: {
-			name: 'PHP',
-			color: '#4f5d95',
-		},
-	},
-	{
-		repo: 'code4romania/civiclabs.ro',
-		description: `Website for the Civic Labs program`,
-		primaryLanguage: {
-			name: 'PHP',
-			color: '#4f5d95',
-		},
-	},
-	{
-		repo: 'code4romania/code4.ro',
-		description: `Code for Romania's public website`,
-		primaryLanguage: {
-			name: 'HTML',
-			color: '#e34c26',
-		},
-	},
-	{
-		repo: 'code4romania/cetetine.ro',
-		description: `A tool that enables professional groups, NGOs or individuals to signal various social blockages that they are facing.`,
-		primaryLanguage: {
-			name: 'HTML',
-			color: '#e34c26',
-		},
-	},
-	{
-		repo: 'code4romania/vot-diaspora',
-		description: `Guide that helps Romanians living abroad to register and vote in parliamentary and presidential elections`,
-		primaryLanguage: {
-			name: 'JavaScript',
-			color: '#f1e05a',
-		},
-	},
-	{
-		repo: 'code4romania/inlar',
-		description: `Website that maps NGOs providing legal services to refugees.`,
-		primaryLanguage: {
-			name: 'PHP',
-			color: '#4f5d95',
-		},
-	},
+    {
+        repo: 'code4romania/cms',
+        description: `Preconfigured CMS with a modular layout`,
+        primaryLanguage: {
+            name: 'PHP',
+            color: '#4f5d95',
+        },
+    },
+    {
+        repo: 'code4romania/civiclabs.ro',
+        description: `Website for the Civic Labs program`,
+        primaryLanguage: {
+            name: 'PHP',
+            color: '#4f5d95',
+        },
+    },
+    {
+        repo: 'code4romania/code4.ro',
+        description: `Code for Romania's public website`,
+        primaryLanguage: {
+            name: 'HTML',
+            color: '#e34c26',
+        },
+    },
+    {
+        repo: 'code4romania/cetetine.ro',
+        description: `A tool that enables professional groups, NGOs or individuals to signal various social blockages that they are facing.`,
+        primaryLanguage: {
+            name: 'HTML',
+            color: '#e34c26',
+        },
+    },
+    {
+        repo: 'code4romania/vot-diaspora',
+        description: `Guide that helps Romanians living abroad to register and vote in parliamentary and presidential elections`,
+        primaryLanguage: {
+            name: 'JavaScript',
+            color: '#f1e05a',
+        },
+    },
+    {
+        repo: 'code4romania/inlar',
+        description: `Website that maps NGOs providing legal services to refugees.`,
+        primaryLanguage: {
+            name: 'PHP',
+            color: '#4f5d95',
+        },
+    },
 ];
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,77 +1,77 @@
 <template>
-	<div
-		class="items-center w-full min-h-screen bg-blue-800 lg:justify-center bg-pattern"
-	>
-		<div class="col-span-12 text-center text-blue-100">
-			<div class="relative block my-5 leading-none">
-				<h1
-					class="relative inline-block overflow-hidden font-semibold whitespace-no-wrap error-code"
-					:data-code="error.statusCode"
-					v-text="error.statusCode"
-				/>
-			</div>
-			<div class="mt-16">
-				<BaseButton type="nuxt" href="/">
-					<arrow-left-icon
-						size="1.5x"
-						class="mr-2"
-						stroke-width="1"
-					/>
-					<span v-text="$t('button.home')" />
-				</BaseButton>
-			</div>
-		</div>
-	</div>
+    <div
+        class="items-center w-full min-h-screen bg-blue-800 lg:justify-center bg-pattern"
+    >
+        <div class="col-span-12 text-center text-blue-100">
+            <div class="relative block my-5 leading-none">
+                <h1
+                    class="relative inline-block overflow-hidden font-semibold whitespace-no-wrap error-code"
+                    :data-code="error.statusCode"
+                    v-text="error.statusCode"
+                />
+            </div>
+            <div class="mt-16">
+                <BaseButton type="nuxt" href="/">
+                    <arrow-left-icon
+                        size="1.5x"
+                        class="mr-2"
+                        stroke-width="1"
+                    />
+                    <span v-text="$t('button.home')" />
+                </BaseButton>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	import { ArrowLeftIcon } from 'vue-feather-icons';
+    import { ArrowLeftIcon } from 'vue-feather-icons';
 
-	export default {
-		components: {
-			ArrowLeftIcon,
-		},
-		props: {
-			error: {
-				type: Object,
-			},
-		},
-	};
+    export default {
+        components: {
+            ArrowLeftIcon,
+        },
+        props: {
+            error: {
+                type: Object,
+            },
+        },
+    };
 </script>
 
 <style lang="scss" scoped>
-	.error-code {
-		text-shadow: 1px 2px 3px #000;
-		line-height: 0.79;
-		font-size: 25vw;
+    .error-code {
+        text-shadow: 1px 2px 3px #000;
+        line-height: 0.79;
+        font-size: 25vw;
 
-		&::before {
-			text-shadow: -0.5vw -0.5vw #17263f, 0.5vw 0.5vw #17263f;
-			animation: glitch 5s infinite alternate-reverse;
-			animation-delay: 0.5s;
-			clip: rect(0, 1px, 0, 0);
-			content: attr(data-code);
-			font-weight: inherit;
-			position: absolute;
-			color: currentColor;
-			left: -2px;
-			top: -2px;
-		}
-	}
+        &::before {
+            text-shadow: -0.5vw -0.5vw #17263f, 0.5vw 0.5vw #17263f;
+            animation: glitch 5s infinite alternate-reverse;
+            animation-delay: 0.5s;
+            clip: rect(0, 1px, 0, 0);
+            content: attr(data-code);
+            font-weight: inherit;
+            position: absolute;
+            color: currentColor;
+            left: -2px;
+            top: -2px;
+        }
+    }
 
-	@keyframes glitch {
-		$maxClipHeight: 25;
-		$steps: 20;
+    @keyframes glitch {
+        $maxClipHeight: 25;
+        $steps: 20;
 
-		@for $step from 0 through $steps {
-			#{percentage($step * (1 / $steps))} {
-				clip: rect(
-					random($maxClipHeight) + vmin,
-					100vw,
-					random($maxClipHeight) + vmin,
-					0
-				);
-			}
-		}
-	}
+        @for $step from 0 through $steps {
+            #{percentage($step * (1 / $steps))} {
+                clip: rect(
+                    random($maxClipHeight) + vmin,
+                    100vw,
+                    random($maxClipHeight) + vmin,
+                    0
+                );
+            }
+        }
+    }
 </style>
