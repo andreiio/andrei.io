ecb26fbb92366669c6160d1ecc8d013f0e74abda
Andrei Ioniță <hi@andrei.io>
2020-08-07 23:47:41 +0100

    Cleanup

--- mixins/work.js
+++ mixins/work.js
@@ -1,32 +1,32 @@
 function getSlugFromResourcePath(resourcePath) {
-	return resourcePath.match(/([^\/:\\*"<>|]+)\.md$/i)[1];
+    return resourcePath.match(/([^\/:\\*"<>|]+)\.md$/i)[1];
 }
 
 export default {
-	computed: {
-		projects() {
-			const pattern = new RegExp(
-				`^.\\/${this.$i18n.locale}\\/work\\/(.+)\\.md$`
-			);
+    computed: {
+        projects() {
+            const pattern = new RegExp(
+                `^.\\/${this.$i18n.locale}\\/work\\/(.+)\\.md$`
+            );
 
-			let items = require
-				.context(`~/content/`, true, /\.md$/)
-				.keys()
-				.filter(key => key.match(pattern) !== null)
-				.map(key => {
-					let item = require(`~/content/${key.replace('./', '')}`);
+            let items = require
+                .context(`~/content/`, true, /\.md$/)
+                .keys()
+                .filter(key => key.match(pattern) !== null)
+                .map(key => {
+                    let item = require(`~/content/${key.replace('./', '')}`);
 
-					item.meta.slug = getSlugFromResourcePath(
-						item.meta.resourcePath
-					);
+                    item.meta.slug = getSlugFromResourcePath(
+                        item.meta.resourcePath
+                    );
 
-					item.meta.permalink = `/work/${item.meta.slug}/`;
+                    item.meta.permalink = `/work/${item.meta.slug}/`;
 
-					return item;
-				});
+                    return item;
+                });
 
-			// Return projects ordered by year desc
-			return this.orderBy(items, 'attributes.year', -1);
-		},
-	},
+            // Return projects ordered by year desc
+            return this.orderBy(items, 'attributes.year', -1);
+        },
+    },
 };
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,68 +1,68 @@
 <template>
-	<div class="container gap-20 py-16 pt-48 lg:grid-cols-2">
-		<h1 class="h1 lg:col-span-2 " v-text="md.attributes.title" />
+    <div class="container gap-20 py-16 pt-48 lg:grid-cols-2">
+        <h1 class="h1 lg:col-span-2 " v-text="md.attributes.title" />
 
-		<component :is="md.vue.component" />
+        <component :is="md.vue.component" />
 
-		<form
-			class="flex flex-wrap"
-			:action="localePath('/thanks/')"
-			method="post"
-			name="contact"
-			netlify-honeypot="bot-field"
-			netlify
-		>
-			<label class="hidden">
-				{{ $t('form.botField') }}
-				<input name="bot-field" />
-			</label>
+        <form
+            class="flex flex-wrap"
+            :action="localePath('/thanks/')"
+            method="post"
+            name="contact"
+            netlify-honeypot="bot-field"
+            netlify
+        >
+            <label class="hidden">
+                {{ $t('form.botField') }}
+                <input name="bot-field" />
+            </label>
 
-			<input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="contact" />
 
-			<FormInput
-				:label="$t('form.name')"
-				type="text"
-				name="name"
-				required
-			/>
+            <FormInput
+                :label="$t('form.name')"
+                type="text"
+                name="name"
+                required
+            />
 
-			<FormInput
-				:label="$t('form.email')"
-				type="email"
-				name="email"
-				required
-			/>
+            <FormInput
+                :label="$t('form.email')"
+                type="email"
+                name="email"
+                required
+            />
 
-			<FormInput
-				:label="$t('form.message')"
-				type="textarea"
-				name="message"
-				required
-			/>
+            <FormInput
+                :label="$t('form.message')"
+                type="textarea"
+                name="message"
+                required
+            />
 
-			<FormButton v-text="$t('form.submit')" />
-		</form>
-	</div>
+            <FormButton v-text="$t('form.submit')" />
+        </form>
+    </div>
 </template>
 
 <script>
-	import metaMixin from '~/mixins/meta.js';
+    import metaMixin from '~/mixins/meta.js';
 
-	export default {
-		data: () => ({
-			md: {},
-		}),
-		head() {
-			return {
-				title: this.md.attributes.title,
-				...metaMixin({
-					title: `${this.md.attributes.title} — ${this.$t('title')}`,
-					description: this.md.attributes.description,
-				}),
-			};
-		},
-		created() {
-			this.md = require(`~/content/${this.$i18n.locale}/contact.md`);
-		},
-	};
+    export default {
+        data: () => ({
+            md: {},
+        }),
+        head() {
+            return {
+                title: this.md.attributes.title,
+                ...metaMixin({
+                    title: `${this.md.attributes.title} — ${this.$t('title')}`,
+                    description: this.md.attributes.description,
+                }),
+            };
+        },
+        created() {
+            this.md = require(`~/content/${this.$i18n.locale}/contact.md`);
+        },
+    };
 </script>
